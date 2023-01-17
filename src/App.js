dfd1761f4fcf090d44b65e43ad02fc94d3df6721
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-01-17 15:09:14 +0200

    initialize components folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Task from './components/Task';
 
 function App() {
   return (
     <div className="App">
+      <Task />
 
     </div>
   );
---
+++ src/components/Task.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Task = () => {
+    return (
+        <div>
+            {"test"}
+        </div>
+      );
+};
+
+export default Task;
