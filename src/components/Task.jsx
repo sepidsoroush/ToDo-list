cb030c2d016c4d6b0889282eee44fe57af504064
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-01-18 10:41:00 +0200

    create default structure of Task box

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import {SquareCheck, Square, Trash, Edit} from 'tabler-icons-react';
 
-const Task = () => {
+const Task = ({item , checkBox , removeItem, editItem , dateItem}) => {
     return (
-        <div>
-            
+        <div className="task-container">
+            <div className="task-border">
+                <Square/>
+                <p>task name</p>
+                <p className="date">01/23</p>
+            </div>
+            <Trash/>
+            <Edit/>
         </div>
       );
 };
