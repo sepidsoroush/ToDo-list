3a01fb15b3c38ab445727744ed067a92d6aec2d7
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-01-18 10:56:27 +0200

    create AddTask structure

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import {Plus, Calendar} from 'tabler-icons-react';
 
 const AddTask = () => {
     return (
         <div>
-            
+            <Plus/>
+            <p>e.g. buy flower</p>
+            <Calendar/>
         </div>
       );
 };
