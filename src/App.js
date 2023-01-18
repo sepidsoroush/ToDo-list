52963d9f1d9cd4a82b0b15ce1f44259679bf500e
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-01-18 10:57:11 +0200

    add AddTask.jsx to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import {SquareCheck, Square, Trash, Edit, Plus, Calendar, Sun, Moon} from 'tabler-icons-react';
 import Task from './components/Task';
+import AddTask from './components/AddTask';
 
 function App() {
   return (
     <div className="App">
+      <AddTask/>
       <Task />
-
     </div>
   );
 }
