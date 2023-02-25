8b779d1f80c24f8e622b9e314c66e04a5bf14ca4
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-02-25 19:09:43 +0200

    create Task structure

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,23 +2,39 @@ import React from "react";
 import {Plus, Calendar , SquareCheck, Square, Trash, Edit} from 'tabler-icons-react';
 
 
-const TaskList = ({item , checkBox , removeItem, editItem , dateItem}) => {
+const TaskList = ({items , checkBox , removeItem, editItem}) => {
     return (
         <div>
-            <div>
-                <div>
-                    <Square/>
-                    <p>task name</p>
-                    <p>01/23</p>
-                </div>
-                <Trash/>
-                <Edit/>
-            </div>
-            <div>
-                <Plus/>
-                <p>e.g. buy flower</p>
-                <Calendar/>
-            </div>
+            {items.map((item)=>{
+                const {id , title , date} = item;
+                return(
+                    <article key={id}>
+                        <div>
+                            {checkBox? <SquareCheck/> : <Square />}
+                        </div>
+                        <div>
+                            <p>{title}</p>
+                            <p>{date}</p>
+                        </div>
+                        <div>
+                            <button
+                            type="button"
+                            onClick={()=>editItem(id)}
+                            >
+                                <Edit />
+                            </button>
+                            <button
+                            type="button"
+                            onClick={()=>removeItem(id)}
+                            >
+                                <Trash />
+                            </button>
+                        </div>
+                        
+
+                    </article>
+                )
+            })}
         </div>
       );
 };
