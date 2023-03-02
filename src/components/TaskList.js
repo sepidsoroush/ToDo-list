9bbde9e06499b0229f60a6c8891bb701a2d4b24e
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-03-02 18:15:48 +0200

    Add style to completed tasks

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -8,9 +8,17 @@ const TaskList = ({items , removeItem, editItem , checkItem}) => {
                 const {id , title ,time , status} = item;
                 return(
                     <article 
-                    className='flex flex-row justify-between items-center w-full my-2 py-2 border rounded shadow-md'
+                    
+                    className = {
+                        status ? 
+                        'flex flex-row justify-between items-center w-full my-2 py-2 border rounded shadow-md bg-gray-100 opacity-70'
+                        :'flex flex-row justify-between items-center w-full my-2 py-2 border rounded shadow-md bg-white'}
+                    
                     key={id}>
-                        <div className='flex flex-row flex-nowrap items-center w-full'>
+                        <div 
+                        className='flex flex-row flex-nowrap items-center w-full'
+                        
+                        >
                             <button
                             className='m-1'
                             type="button"
@@ -19,7 +27,7 @@ const TaskList = ({items , removeItem, editItem , checkItem}) => {
                                 { status? <SquareCheck className='stroke-1'/> : <Square className='stroke-1' />}
                             </button>
                             <div className='flex flex-row flex-nowrap justify-between items-center w-full'>
-                                <div className='text-sm font-semibold'>{title}</div>
+                                <div className={status? 'font-light' : 'font-medium'}>{title}</div>
                                 <div className='text-xs text-gray-500 px-2'>{time}</div>
                             </div>
                         </div>
@@ -29,13 +37,13 @@ const TaskList = ({items , removeItem, editItem , checkItem}) => {
                             type="button"
                             onClick={()=>editItem(id)}
                             >
-                                <Edit className='stroke-1 text-emerald-600 m-1' />
+                                <Edit className={status? 'stroke-1 text-gray-600 m-1' :'stroke-1 text-emerald-600 m-1'} />
                             </button>
                             <button
                             type="button"
                             onClick={()=>removeItem(id)}
                             >
-                                <Trash className='stroke-1 text-red-600 m-1' />
+                                <Trash className={status? 'stroke-1 text-gray-600 m-1' :'stroke-1 text-red-600 m-1'} />
                             </button>
                         </div>
                     </article>
